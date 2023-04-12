a123b86c3101ccc32c9b247366dd81fcbae7e5ff
Bruno Rodrigues <brunob1990@gmail.com>
2023-04-12 17:26:00 -0300

    init

---
+++ src/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+/*if ( !function_exists( 'user' ) ) {
+	function user(): User|null
+	{
+		return Auth::user();
+	}
+}*/
+
+if ( !function_exists( 'helperTeste' ) ) {
+	function helperTeste(): string
+	{
+		return 'helperTeste';
+	}
+}
