12304144720474a096dfa650989a38feb1b219d8
Bruno Rodrigues <brunob1990@gmail.com>
2023-09-26 10:24:44 -0300

    feat: Add Api Telegram - sendMessage

---
+++ src/Api/Telegram.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace RedeCauzzoMais\Api;
+
+use Illuminate\Support\Facades\Http;
+
+class Telegram
+{
+    public static int $timeout = 0;
+
+    public static function sendMessage( string $message, ?int $chatId = null ): bool
+    {
+        $endpoint = config( 'local.telegram.endpoint' );
+        $token    = config( 'local.telegram.token' );
+
+        if ( empty( $chatId ) ) {
+            $chatId = config( 'local.telegram.chat_id' );
+        }
+
+        $title   = config( 'app.name' );
+        $message = str_replace( '<br>', "\n", $message );
+
+        $message = "<b>$title</b>\n" . $message;
+
+        if ( mb_strlen( $message ) > 4096 ) {
+            $message = substr( $message, 0, 4096 );
+        }
+
+        $response = Http::timeout( static::$timeout )->get( $endpoint . $token . '/sendMessage', [
+            'parse_mode' => 'HTML',
+            'text'       => $message,
+            'chat_id'    => $chatId
+        ] );
+
+        return (bool) $response->status();
+    }
+}
